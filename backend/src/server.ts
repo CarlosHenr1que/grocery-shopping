7648a6bd8505ee4185da2eac720f453cac90e437
Carlos Borges <carlosh.mb@outlook.com>
2025-04-21 11:12:46 +0100

    chore: add start script

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express();
+
+const start = async () => {
+  try {
+    app.listen(3000, () => {
+      console.log("Server started on port 3000");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
