3a44abb7327eba9b9e2c9f15162256b53bc76406
Carlos Borges <carlosh.mb@outlook.com>
2025-05-09 15:25:21 +0100

    chore: add dotenv

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,14 @@
 /* eslint-disable no-console */
+import dotenv from "dotenv";
 import { MongoHelper } from "./infra/repositories/mongo/helpers/mongo-helper";
 import app from "./interface/http/express";
+dotenv.config();
 
 MongoHelper.connect("mongodb://localhost:27017/grocery")
   .then(() => {
     try {
-      app.listen(3000, () => {
-        console.log("Server started on port 3000");
+      app.listen(process.env.PORT, () => {
+        console.log(`Server started on port ${process.env.PORT}`);
       });
     } catch (error) {
       console.log(error);
