bd63657b9bd84a1a91a3ac2a93f83931d985dd51
Carlos Borges <carlosh.mb@outlook.com>
2025-05-12 15:53:30 +0100

    refactor: change orders route

--- backend/src/interface/routes/order-routes.ts
+++ backend/src/interface/routes/order-routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { adaptRoute } from "../adapters/express-route-adapter";
 import { OrderMongoRepository } from "../../infra/repositories/mongo/mongo-order-repository";
 import { AddOrder } from "../../use-cases/add-order";
-import { AddOrderController } from "../controllers/order/add-order";
+import { AddOrderController } from "../controllers/orders/add-order";
 import { ProductsMongoRepository } from "../../infra/repositories/mongo/products/mongo-products-repository";
 const router = Router();
 
@@ -10,5 +10,5 @@ const orderRepository = new OrderMongoRepository();
 const productsRepository = new ProductsMongoRepository();
 const addOrder = new AddOrder(orderRepository, productsRepository);
 const addOrderController = new AddOrderController(addOrder);
-router.post("/order", adaptRoute(addOrderController));
+router.post("/orders", adaptRoute(addOrderController));
 export { router as orderRouter };
