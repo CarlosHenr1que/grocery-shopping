f462a874d0daa1e9038dda98737efc4d01b79ea4
Carlos Borges <carlosh.mb@outlook.com>
2025-06-14 21:14:12 +0100

    fix: prettier

--- apps/backend/src/interface/routes/order-routes.ts
+++ apps/backend/src/interface/routes/order-routes.ts
@@ -1,11 +1,12 @@
-import { Router } from "express";
-import { adaptRoute } from "../adapters/express-route-adapter";
-import { OrderMongoRepository } from "../../infra/repositories/mongo/mongo-order-repository";
-import { AddOrder } from "../../use-cases/add-order";
-import { AddOrderController } from "../controllers/orders/add-order";
-import { ProductsMongoRepository } from "../../infra/repositories/mongo/products/mongo-products-repository";
-import { GetOrdersController } from "../controllers/orders/get-orders";
-import { GetOrders } from "../../use-cases/get-orders";
+import { Router } from 'express';
+import { adaptRoute } from '../adapters/express-route-adapter';
+import { OrderMongoRepository } from '../../infra/repositories/mongo/mongo-order-repository';
+import { AddOrder } from '../../use-cases/add-order';
+import { AddOrderController } from '../controllers/orders/add-order';
+import { ProductsMongoRepository } from '../../infra/repositories/mongo/products/mongo-products-repository';
+import { GetOrdersController } from '../controllers/orders/get-orders';
+import { GetOrders } from '../../use-cases/get-orders';
+
 const router = Router();
 
 const orderRepository = new OrderMongoRepository();
@@ -14,6 +15,8 @@ const addOrder = new AddOrder(orderRepository, productsRepository);
 const getOrders = new GetOrders(orderRepository);
 const addOrderController = new AddOrderController(addOrder);
 const getOrdersController = new GetOrdersController(getOrders);
-router.post("/orders", adaptRoute(addOrderController));
-router.get("/orders", adaptRoute(getOrdersController));
+
+router.post('/orders', adaptRoute(addOrderController));
+router.get('/orders', adaptRoute(getOrdersController));
+
 export { router as orderRouter };
