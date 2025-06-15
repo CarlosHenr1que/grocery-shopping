e9b97c156c3b5e1ac8e4009cc291a9b4f6ec5b59
Carlos Borges <carlosh.mb@outlook.com>
2025-06-15 20:31:27 +0100

    feat(frontend): config e2e test

--- apps/frontend-e2e/src/e2e/app.cy.ts
+++ apps/frontend-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,7 @@
-import { getGreeting } from '../support/app.po';
-
 describe('@grocery-shopping/frontend-e2e', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains(/Welcome/);
+  it('should see home page', () => {
+    cy.contains('Home').should('be.visible');
   });
 });
