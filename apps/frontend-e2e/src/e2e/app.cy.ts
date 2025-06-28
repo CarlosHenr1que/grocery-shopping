f10c596e347603bf1f32d01e47176dc18936729b
Carlos Borges <carlosh.mb@outlook.com>
2025-06-28 19:12:44 +0100

    test(frontend): fix home e2e test

--- apps/frontend-e2e/src/e2e/app.cy.ts
+++ apps/frontend-e2e/src/e2e/app.cy.ts
@@ -2,6 +2,6 @@ describe('@grocery-shopping/frontend-e2e', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should see home page', () => {
-    cy.contains('Home').should('be.visible');
+    cy.get('[data-testid="product-card"]').should('be.visible');
   });
 });
